47aa0168038f398c11cc1ed54e0cc7bfac605456
alexkokkinos <kokkinos.alex@gmail.com>
2016-10-01 20:12:05 -0400

    Added elastic loadbalancer

--- terraform/infra/infra.tf
+++ terraform/infra/infra.tf
@@ -227,3 +227,38 @@ resource "aws_route_table_association" "two" {
   subnet_id = "${aws_subnet.public1b.id}"
   route_table_id = "${aws_route_table.internet_route.id}"
 }
+
+resource "aws_elb" "siteelb" {
+  name = "terraform-elb"
+
+  access_logs {
+    bucket = "siteelb"
+    bucket_prefix = "elb"
+    interval = 60
+  }
+
+  listener {
+    instance_port = 8000
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8000/"
+    interval = 30
+  }
+
+  instances = ["${aws_instance.web01.id}", "${aws_instance.web02.id}"]
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+  subnets = ["${aws_subnet.public1a.id}", "${aws_subnet.public1b.id}"]
+
+  tags {
+    Name = "terraform-elb"
+  }
+}
